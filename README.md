222a6211ac77c5adc6419d1cf5123dd1e58ac7de
AISLIN JOHN <johnan@vcu.edu>
2023-11-12 20:15:14 -0500

    .

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# cmsc508-fa2023-hw7-template
+# Homework 7 - CMSC508 - Fall 2023
+
+## Project Overview
+This database is designed to support a comprehensive data management system that facilitates the organization and interaction of individuals, their skills, and assigned roles. The primary goal is to efficiently manage and connect these entities, allowing for seamless retrieval and utilization of relevant information. The project involves three main tables: People, Skills, and Roles, each playing a vital role in capturing and organizing essential data within the system.
+
+## Homework tasks
+
+1. Install the necessary python tools and libraries
+2. Verify that the tools and libraries are installed.
+3. Verify that you can log into the phpMyAdmin site.
+4. Verify that your computer can connect to the mySql server.
+5. Explore queries and result sets using pandas
+6. Explore Quarto as a tool for writing out tables.
+7. Explore and document a new database that some provided to you.
+8. Complete the *hw6.qmd* scaffold and render the report
+
+
+## SQL Queries
+To interact with the database, a series of SQL queries have been provided in the documentation. These queries cover a range of tasks, from populating tables to extracting specific information about people, skills, and roles. Refer to the documentation for a detailed list of SQL queries and their applications.
+
+## Files and folders
+
+All necessary files are stored in the *./hw7* folder.
--- hw7/hw7-ddl.sql
+++ hw7/hw7-ddl.sql
@@ -18,6 +18,10 @@
 
 SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS people;
+DROP TABLE IF EXISTS skills;
+DROP TABLE IF EXISTS peopleroles;
+DROP TABLE IF EXISTS peopleskills;
+DROP TABLE IF EXISTS roles;
 # ... 
 SET FOREIGN_KEY_CHECKS=1;
 
@@ -25,8 +29,19 @@ SET FOREIGN_KEY_CHECKS=1;
 # Create skills( id,name, description, tag, url, time_commitment)
 # ID, name, description and tag cannot be NULL. Other fields can default to NULL.
 # tag is a skill category grouping.  You can assign it based on your skill descriptions.
-# time committment offers some sense of how much time was required (or will be required) to gain the skill.
+# time commitment offers some sense of how much time was required (or will be required) to gain the skill.
 # You can assign the skill descriptions.  Please be creative!
+
+drop table if exists skills;
+CREATE TABLE skills(
+    skills_id int not null,
+    skills_name varchar(255) not null,
+    skills_description varchar(255) not null DEFAULT '(default description)',
+    skills_tag varchar(255) not null,
+    skills_url varchar(255),
+    skills_time_commitment int,
+    primary key(skills_id)
+);
 
 
 # Section 3
@@ -35,14 +50,35 @@ SET FOREIGN_KEY_CHECKS=1;
 # You can assign skill names.  Please be creative!
 
 
+insert into skills (skills_id, skills_name, skills_tag) values
+  (1, 'rock climbing', 'Skill 1'),
+  (2, 'mind reading', 'Skill 2'),
+  (3, 'dancing', 'Skill 3'),
+  (4, 'cooking', 'Skill 4'),
+  (5, 'drawing', 'Skill 5'),
+  (6, 'coding in commom lisp', 'Skill 6'),
+  (7, 'singing', 'Skill 7'),
+  (8, 'creating fire from rocks','Skill 8');
+
+
+
+
 # Section 4
 # Create people( id,first_name, last_name, email, linkedin_url, headshot_url, discord_handle, brief_bio, date_joined)
 # ID cannot be null, Last name cannot be null, date joined cannot be NULL.
 # All other fields can default to NULL.
 
+drop table if exists people;
 CREATE TABLE people (
-    people_id int,
+    people_id int NOT NULL,
     people_last_name varchar(256) NOT NULL,
+    people_first_name varchar(256),
+    email varchar(256),
+    linkedin_url varchar(256),
+    headshot_url varchar(256),
+    discord_handle varchar(256),
+    brief_bio varchar(256),
+    date_joined date NOT NULL,
     PRIMARY KEY (people_id)
 );
 
@@ -51,12 +87,35 @@ CREATE TABLE people (
 # Their last names must exactly be “Person 1”, “Person 2”, etc.
 # Other fields are for you to assign.
 
-insert into people (people_id,people_last_name) values (1,'Person 1');
+
+INSERT INTO people (people_id, people_last_name, people_first_name, email, linkedin_url, headshot_url, discord_handle, brief_bio, date_joined) VALUES
+(1, 'Person 1', 'Albert', 'albert@example.com', 'https://www.linkedin.com/albert', 'https://example.com/albert.jpg', 'Albert#1234', 'Passionate about technology', '2023-01-01'),
+(2, 'Person 2', 'Bianca', 'bianca@example.com', 'https://www.linkedin.com/bianca', 'https://example.com/bianca.jpg', 'Bianca#5678', 'Enthusiastic learner', '2023-02-15'),
+(3, 'Person 3', 'Charlie', 'charlie@example.com', 'https://www.linkedin.com/charlie', 'https://example.com/charlie.jpg', 'Charlie#9101', 'Creative thinker', '2023-03-30'),
+(4, 'Person 4', 'Diana', 'diana@example.com', 'https://www.linkedin.com/diana', 'https://example.com/diana.jpg', 'Diana#1122', 'Aspiring professional', '2023-05-12'),
+(5, 'Person 5', 'Edward', 'edward@example.com', 'https://www.linkedin.com/edward', 'https://example.com/edward.jpg', 'Edward#3344', 'Passionate about arts', '2023-06-25'),
+(6, 'Person 6', 'Fiona', 'fiona@example.com', 'https://www.linkedin.com/fiona', 'https://example.com/fiona.jpg', 'Fiona#5566', 'Tech-savvy and creative', '2023-08-08'),
+(7, 'Person 7', 'George', 'george@example.com', 'https://www.linkedin.com/george', 'https://example.com/george.jpg', 'George#7788', 'Experienced professional', '2023-09-20'),
+(8, 'Person 8', 'Hannah', 'hannah@example.com', 'https://www.linkedin.com/hannah', 'https://example.com/hannah.jpg', 'Hannah#9900', 'Innovative thinker', '2023-10-05'),
+(9, 'Person 9', 'Ian', 'ian@example.com', 'https://www.linkedin.com/ian', 'https://example.com/ian.jpg', 'Ian#1122', 'Curious and analytical', '2023-11-18'),
+(10, 'Person 10', 'Jessica', 'jessica@example.com', 'https://www.linkedin.com/jessica', 'https://example.com/jessica.jpg', 'Jessica#3344', 'Passionate about design', '2023-12-01');
 
 
 # Section 6
 # Create peopleskills( id, skills_id, people_id, date_acquired )
 # None of the fields can ba NULL. ID can be auto_increment.
+
+drop table if exists peopleskills;
+create table peopleskills( 
+    id int auto_increment,
+    skills_id int,
+    people_id int,
+    date_acquired date default(current_date),
+    primary key (id),
+    foreign key(skills_id) references skills (skills_id),
+    foreign key(people_id) references people (people_id),
+     unique(people_id,skills_id)
+    );
 
 
 # Section 7
@@ -73,24 +132,72 @@ insert into people (people_id,people_last_name) values (1,'Person 1');
 # Person 10 has skills 1,4,5;
 # Note that no one has yet acquired skills 7 and 8.
  
+insert into peopleskills (people_id, skills_id) values
+(1,1),
+(1,3),
+(1,6),
+(2,3),
+(2,4),
+(2,5),
+(3,1),
+(3,5),
+(5,3),
+(5,6),
+(6,2),
+(6,3),
+(6,4),
+(7,3),
+(7,5),
+(7,6),
+(8,1),
+(8,3),
+(8,5),
+(8,6),
+(9,2),
+(9,5),
+(9,6),
+(10,1),
+(10,4),
+(10,5);
 
 # Section 8
 # Create roles( id, name, sort_priority )
 # sort_priority is an integer and is used to provide an order for sorting roles
 
-
+drop table if exists roles;
+create table roles(
+     id int,
+     name varchar(255),
+     sort_priority int,
+     primary key(id) 
+);
 
 # Section 9
 # Populate roles
 # Designer, Developer, Recruit, Team Lead, Boss, Mentor
 # Sort priority is assigned numerically in the order listed above (Designer=10, Developer=20, Recruit=30, etc.)
 
+INSERT INTO roles (id, name, sort_priority) VALUES
+(1, 'Designer', 10),
+(2, 'Developer', 20),
+(3, 'Recruit', 30),
+(4, 'Team Lead', 40),
+(5, 'Boss', 50),
+(6, 'Mentor', 60);
 
 
 # Section 10
 # Create peopleroles( id, people_id, role_id, date_assigned )
 # None of the fields can be null.  ID can be auto_increment
 
+drop table if exists peopleroles;
+create table peopleroles(
+     id int not null auto_increment,
+     people_id int not null,
+     role_id int not null,
+     date_assigned int not null, 
+     primary key(id)
+);     
 
 
 # Section 11
@@ -106,3 +213,20 @@ insert into people (people_id,people_last_name) values (1,'Person 1');
 # Person 9 is Developer
 # Person 10 is Developer and Designer
 
+
+INSERT INTO peopleroles (people_id, role_id, date_assigned) VALUES
+(1, 2, UNIX_TIMESTAMP()),
+(2, 5, UNIX_TIMESTAMP()),
+(2, 6, UNIX_TIMESTAMP()),
+(3, 2, UNIX_TIMESTAMP()),
+(3, 4, UNIX_TIMESTAMP()),
+(4, 3, UNIX_TIMESTAMP()),
+(5, 3, UNIX_TIMESTAMP()),
+(6, 2, UNIX_TIMESTAMP()),
+(6, 1, UNIX_TIMESTAMP()),
+(7, 1, UNIX_TIMESTAMP()),
+(8, 1, UNIX_TIMESTAMP()),
+(8, 4, UNIX_TIMESTAMP()),
+(9, 2, UNIX_TIMESTAMP()),
+(10, 2, UNIX_TIMESTAMP()),
+(10, 1, UNIX_TIMESTAMP());
